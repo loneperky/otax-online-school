ac3dbff0ce24b885234247baefed5f82b21ad4e6
loneperky <loneperky@gmail.com>
2025-01-29 18:44:35 -0800

    Just created the Developer incubator

--- Frontend/src/pages/ContactPage.jsx
+++ Frontend/src/pages/ContactPage.jsx
@@ -1,8 +1,18 @@
-import { Link } from "react-router-dom"
-import { Contact, Footer } from "./construct"
-import '../styles/contact.css'
-function ContactPage(){
-  return(
+import { Link } from "react-router-dom";
+import {
+  FaWhatsapp,
+  FaLink,
+  FaPhone,
+  FaFacebook,
+  FaInstagram,
+  FaTiktok,
+  FaTwitter,
+  FaEnvelope,
+} from "react-icons/fa";
+import { Contact, Footer } from "./construct";
+import "../styles/contact.css";
+function ContactPage() {
+  return (
     <>
       <div className="overall">
         <div className="head">
@@ -12,56 +22,64 @@ function ContactPage(){
           <div className="con-hero1">
             <button>Get in touch</button>
             <h1>We would like to hear to hear from you.</h1>
-            <p>We're here to help Whether you have questions about our services,need support,or want to learn more about joining our network, feel free to reach out through any of the options below.</p>
+            <p>
+              We're here to help Whether you have questions about our
+              services,need support,or want to learn more about joining our
+              network, feel free to reach out through any of the options below.
+            </p>
           </div>
 
           <div className="con-hero2">
             <div className="container1">
-            <div className="contd">
-              <div className="logo">
-
+              <div className="contd">
+                <div className="log">
+                  <FaWhatsapp />
+                </div>
+                <h3>Chat our support</h3>
+                <p>We are here to help</p>
+                <Link to="whatsapp-link">Chat on Whtasapp</Link>
               </div>
-              <h3>Chat our support</h3>
-              <p>We are here to help</p>
-              <Link to="whatsapp-link">Chat on Whtasapp</Link>
-            </div>
 
-            <div className="contd">
-              <div className="logo">
-
+              <div className="contd">
+                <div className="log">
+                  <FaPhone />
+                </div>
+                <h3>Call us</h3>
+                <p>Mon-Fri from 8am to 5pm</p>
+                <Link to="whatsapp-link">+234-9153383705</Link>
               </div>
-              <h3>Call us</h3>
-              <p>Mon-Fri from 8am to 5pm</p>
-              <Link to="whatsapp-link">+2349153383705</Link>
             </div>
-            </div>
-           
 
             <div className="container2">
-            <div className="contd">
-              <div className="logo">
-
+              <div className="contd">
+                <div className="log">
+                  <FaEnvelope />
+                </div>
+                <h3>Send us a mail</h3>
+                <p>Speak to us via mail</p>
+                <Link to="contact@osdin.net">Contact@osdin.net</Link>
               </div>
-              <h3>Send us a mail</h3>
-              <p>Speak to us via mail</p>
-              <Link to="contact@osdin.net">Contact@osdin.net</Link>
-            </div>
 
-            <div className="contd">
-              <div className="logo">
-
+              <div className="contd">
+                <div className="log">
+                  <FaLink />
+                </div>
+                <h3>Social Media</h3>
+                <p>Connect with us</p>
+                <div className="logg">
+                  <FaFacebook />
+                  <FaInstagram />
+                  <FaTiktok />
+                  <FaTwitter />
+                </div>
               </div>
-              <h3>Social Media</h3>
-              <p>Connect with us</p>
             </div>
-            </div>
-           
           </div>
         </div>
       </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default ContactPage
+export default ContactPage;
--- Frontend/src/pages/Developer.jsx
+++ Frontend/src/pages/Developer.jsx
@@ -1,24 +1,84 @@
 import React from 'react'
+import '../styles/developer.css'
+import { FaDev } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { Footer } from './construct'
 
 const Developer = () => {
   return (
     <>
-      <div className="overall-view">
-        <div className="top">
-          <h1>Artificial Intelligence / Machine Learning Software development Incubator</h1>
-          <p>In our incubator program,students will build full-scale AI and ML Powered Software solutions using the skills below, with the top teams winning $1000 each for their innovative projects. </p>
-          <Link to="">Pick a course to learn below: </Link>
-        </div>
-        <div className="top-courses">
-          <div className="couser1">
-            <div className="logg">
-
-            </div>
-            <h3>Front/Backend Web Development (Full stack with JavaScript)</h3>
+    <div className="tota">
+    <div className="overall-view">
+        <div className="view1">
+          <div className="top">
+            <h1>Artificial Intelligence / Machine Learning Software development Incubator</h1>
+            <p>In our incubator program,students will build full-scale AI and ML Powered Software solutions using the skills below, with the top teams winning $1000 each for their innovative projects. </p>
+            <p className='pic'>Pick a course to learn below:</p>
           </div>
+          <div className="top-courses">
+            <div className="couser1">
+              <div className="log
+              ">
+                <FaDev />
+              </div>
+              <h4>Front/Backend Web Development (Full stack with JavaScript)</h4>
+            </div>
+            
+            <div className="couser1">
+              <div className="log">
+              <FaDev />
+              </div>
+              <h4>Artificial Intelligence and Machine Learning (Python Programming) </h4>
+            </div>
+
+            <div className="couser1">
+              <div className="log">
+              <FaDev />
+              </div>
+              <h4>Android/ iOS App Development (Flutter)</h4>
+            </div>
+
+          </div>
+          <div className="top-courses">
+          <div className="couser1">
+              <div className="log">
+              <FaDev />
+              </div>
+              <h4>UI/UX Design (Figma User Interface and User  Experience Design)</h4>
+            </div>
+
+            <div className="couser1">
+              <div className="log">
+              <FaDev />
+              </div>
+              <h4>DevSecops Engineering (Cybersecurity and DevOps)</h4>
+            </div>
+
+            <div className="couser1">
+              <div className="log">
+              <FaDev />
+              </div>
+              <h4>Data Engineering (Data Analysis for Ai and Machine Learning)</h4>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="form">
+          <h1>Get Started Now</h1>
+          <p>Fill in your details to apply now</p>
+          <form>
+            <label htmlFor="Fullname">Fullname</label>
+            <input type="text" autoFocus="on" placeholder='John Doe' required/>
+            <label htmlFor="Email">Email</label>
+            <input type="email" placeholder='johndoe@gmail.com' required/>
+            <button>{ 'Apply Now'}</button>
+          </form>
         </div>
       </div>
+    </div>
+     <Footer />
+    
     </>
   )
 }
